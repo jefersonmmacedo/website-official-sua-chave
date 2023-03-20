03b3f81fce026948397d8f5565923a457570758a
jefersonmmacedo <jefersonmacedowgf@gmail.com>
2023-03-20 17:31:55 -0300

    Vídeo de apresentação da plataforma

--- src/pages/Announce/Announce.jsx
+++ src/pages/Announce/Announce.jsx
@@ -64,7 +64,7 @@ export function Announce() {
             <div className="moviePlay">
                 <div className="box1"></div>
                 <div className="play">
-                    <FaPlay />
+                    <FaPlay onClick={handleOpenModal}/>
                 </div>
                 <div className="box2"></div>
             </div>
@@ -220,7 +220,7 @@ export function Announce() {
             </button>
             <div className="content-modal">
    
-    <iframe width="100%" height="400" src="https://www.youtube-nocookie.com/embed/4lRRANhqnog" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    <iframe width="100%" height="400" src="https://www.youtube.com/embed/spafR9XKFXc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 
             </div>
             </Modal>
